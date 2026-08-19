30a7b0aefea75ffbecba1d4760e453e9e972dad3
[ - Lucas Modesto - ] <130197468+Leppyzin@users.noreply.github.com>
2026-08-18 23:04:40 -0300

    Implement beecrowd 1329 solution in C

--- Aula_05/Exercícios/beecrowd_1329.c
+++ Aula_05/Exercícios/beecrowd_1329.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){
+    int n,ri;
+    
+    while(scanf("%d",&n) && n != 0){
+        int mary = 0;
+        int john = 0;
+    
+        for(int i=0;i < n;i++){
+            scanf("%d",&ri);
+            
+            if(ri == 1){
+                john = john+1;
+            } else {
+                mary = mary+1;
+            }
+        }
+        printf("Mary won %d times and John won %d times\n",mary,john);
+    }
+
+    
+
+    return 0;
+}
